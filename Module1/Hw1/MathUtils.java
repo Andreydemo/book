1ed178252b53da555fbfbc23a571457e37de3df1
Yevgeniy Batulin <Yevgeniy_Batulin@epam.com>
2016-02-22 11:35:22 +0200

    added sub operation
    added div operation
    
    added mul operation
    
    added sqrtBad method
    
    renamed sqrtBad into sqrt

--- Module1/Hw1/MathUtils.java
+++ Module1/Hw1/MathUtils.java
@@ -2,4 +2,20 @@ public class MathUtils {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    public int sqrt(int a) {
+        return (int) Math.sqrt(a);
+    }
 }
