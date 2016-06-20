cba93ec546f17cc0fec4ccaf8af2fd77dd8ae9fa
Yevgeniy Batulin <Yevgeniy_Batulin@epam.com>
2016-06-20 21:41:08 +0300

    3. We need to get gas oven could not be baked without gas

--- Module4/Hw1/bakery/src/Oven.java
+++ Module4/Hw1/bakery/src/Oven.java
@@ -1,6 +1,18 @@
-public class Oven extends BreadFactory {
+public class Oven extends BreadFactory implements Gas {
+    private int levelOfGas;
+
     @Override
     Bread makeBread() {
+        levelOfGas -= 10;
         return new Bread();
+    }
+
+    @Override
+    public int getLevelOfGas() {
+        return levelOfGas;
+    }
+
+    public void setLevelOfGas(int levelOfGas) {
+        this.levelOfGas = levelOfGas;
     }
 }
